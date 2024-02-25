153e9ce67f561c6d08721e849d39bdfc52bb3f89
alecpayos <alec@getdentalray.com>
2024-02-26 02:27:53 +0800

    added assets currently and for future use; changed default next file to portolio hero page;

---
+++ src/app/assets/index.js
@@ -0,0 +1,95 @@
+import googleIcon from './misc/google-icon.svg'
+import googleMapsIcon from './misc/google-maps.svg'
+import icons8PhoneIcon from './misc/icons8-phone.svg'
+import linkedinIcon from './misc/linkedin-icon.svg'
+import chainLinkIcon from './misc/chain-link-icon.svg'
+
+import grabADrinkPreviewIcon from './project-previews/grab-a-drink-preview.png'
+import ohifCustomMprPreviewIcon from './project-previews/ohif-custom-mpr-preview.png'
+import ohifWorklistPreviewIcon from './project-previews/ohif-worklist-preview.png'
+import resumePreviewIcon from './project-previews/resume-preview.png'
+import speccupPreivewIcon from './project-previews/speccup-preivew.png'
+
+import antDesignIcon from './tech-stack-icons/ant-design.svg'
+import appleIcon from './tech-stack-icons/apple.svg'
+import awsApiGatewayIcon from './tech-stack-icons/aws-api-gateway.svg'
+import awsCloudformationIcon from './tech-stack-icons/aws-cloudformation.svg'
+import awsCloudfrontIcon from './tech-stack-icons/aws-cloudfront.svg'
+import awsCloudwatchIcon from './tech-stack-icons/aws-cloudwatch.svg'
+import awsEc2Icon from './tech-stack-icons/aws-ec2.svg'
+import awsIamIcon from './tech-stack-icons/aws-iam.svg'
+import awsRdsIcon from './tech-stack-icons/aws-rds.svg'
+import awsS3Icon from './tech-stack-icons/aws-s3.svg'
+import awsIcon from './tech-stack-icons/aws.svg'
+import bitbucketIcon from './tech-stack-icons/bitbucket.svg'
+import bootstrapIcon from './tech-stack-icons/bootstrap.svg'
+import cssIcon from './tech-stack-icons/css-3.svg'
+import figmaIcon from './tech-stack-icons/figma.svg'
+import flaskIcon from './tech-stack-icons/flask.svg'
+import gitIcon from './tech-stack-icons/git-icon.svg'
+import githubIcon from './tech-stack-icons/github-icon.svg'
+import gitlabIcon from './tech-stack-icons/gitlab.svg'
+import htmlIcon from './tech-stack-icons/html-5.svg'
+import laravelIcon from './tech-stack-icons/laravel.svg'
+import materialUiIcon from './tech-stack-icons/material-ui.svg'
+import microsoftWindowsIcon from './tech-stack-icons/microsoft-windows-icon.svg'
+import mysqlIcon from './tech-stack-icons/mysql-icon.svg'
+import nextjsIcon from './tech-stack-icons/nextjs-icon.svg'
+import nginxIcon from './tech-stack-icons/nginx.svg'
+import phpIcon from './tech-stack-icons/php.svg'
+import postgresqlIcon from './tech-stack-icons/postgresql.svg'
+import pythonIcon from './tech-stack-icons/python.svg'
+import reactIcon from './tech-stack-icons/react.svg'
+import slackIcon from './tech-stack-icons/slack-icon.svg'
+import trelloIcon from './tech-stack-icons/trello.svg'
+import typescriptIcon from './tech-stack-icons/typescript-icon.svg'
+import ubuntuIcon from './tech-stack-icons/ubuntu.svg'
+import vueIcon from './tech-stack-icons/vue.svg'
+
+export {
+    googleIcon,
+    googleMapsIcon,
+    icons8PhoneIcon,
+    linkedinIcon,
+    chainLinkIcon,
+    grabADrinkPreviewIcon,
+    ohifCustomMprPreviewIcon,
+    ohifWorklistPreviewIcon,
+    resumePreviewIcon,
+    speccupPreivewIcon,
+    antDesignIcon,
+    appleIcon,
+    awsApiGatewayIcon,
+    awsCloudformationIcon,
+    awsCloudfrontIcon,
+    awsCloudwatchIcon,
+    awsEc2Icon,
+    awsIamIcon,
+    awsRdsIcon,
+    awsS3Icon,
+    awsIcon,
+    bitbucketIcon,
+    bootstrapIcon,
+    cssIcon,
+    figmaIcon,
+    flaskIcon,
+    gitIcon,
+    githubIcon,
+    gitlabIcon,
+    htmlIcon,
+    laravelIcon,
+    materialUiIcon,
+    microsoftWindowsIcon,
+    mysqlIcon,
+    nextjsIcon,
+    nginxIcon,
+    phpIcon,
+    postgresqlIcon,
+    pythonIcon,
+    reactIcon,
+    slackIcon,
+    trelloIcon,
+    typescriptIcon,
+    ubuntuIcon,
+    vueIcon
+}
